fbf0df962763a74eea762bbcba32e959fceaf1f6
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-01 18:36:24 +0100

    2

--- input.java
+++ input.java
@@ -1,2 +1,24 @@
 import java.util.Scanner;
 
+public class Input
+
+public static void main (Strings[]args)
+system.out.printLn(What is ur name)
+
+scanner scan =kdb scanner
+
+
+
+//
+import java.util.Scanner;
+public class HelloName
+{
+public static void main(String[] args)
+{
+Scanner keyboard = new Scanner(System.in);
+String temp;
+System.out.print("What is your name? ");
+temp = keyboard.nextLine();
+System.out.print("Hello " + temp);
+}
+}
