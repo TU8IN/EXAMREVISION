1014375601829188356f38f6454f20108003df57
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-01 19:04:39 +0100

    2

--- double.java
+++ double.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 class double
 {
-public static void main(String[] args)
-{
+public static void main(String[] args){
+
 Scanner keyboard = new Scanner(System.in);
 double first, second;
 double sum;
@@ -14,3 +14,5 @@ sum = first + second;
 System.out.print("Sum is " + sum);
 }
 }
+
+//[age 66]
