08f33828ed09c67cadea12453babdcbfcce9fb9e
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-12 22:21:49 +0100

    1

---
+++ 2023.java
@@ -0,0 +1,27 @@
+public class Coffee {
+
+    private string name;
+    private int strength;
+    private string origin;
+
+    public class(string name,int strength,string origin)
+    this.name =name
+    this.strength = strrgnth
+    this.origin = origin
+    }
+    public void getname()
+    return name
+
+    public void getstrngth()
+    return strength
+
+    public void origin()
+    return origin
+
+
+    public static void main(String[] args) {
+        system.out,println(tostring)
+    }
+
+    public tostring()
+    return string 
