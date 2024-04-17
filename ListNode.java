b131267b9ed1e3d2d87f1c63fcad635c7d1f8409
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-17 13:24:17 +0100

    2

---
+++ ListNode.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+
+public class ListNode{
+    private String payload;
+    private String next;
+
+    class ListNode(String payload,String next)
+}
+public class StringListNode {
+    
+}
