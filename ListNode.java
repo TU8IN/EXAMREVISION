fa35bfef939521260a36b99e7554df2f87e0b7b1
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-17 13:30:33 +0100

    1

--- ListNode.java
+++ ListNode.java
@@ -4,7 +4,17 @@ public class ListNode{
     private String payload;
     private String next;
 
-    class ListNode(String payload,String next)
+    public ListNode(String payload,String next);
+    this.payload = payload;
+    this.next =next;
+
+    public get String payload()
+        return payload;
+
+    public get String next()
+        return next;
+    
+        
 }
 public class StringListNode {
     
