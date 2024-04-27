ca38d76007feddb1c372c53deee66422a7ad3d25
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-27 17:44:14 +0100

    2

--- House.java
+++ House.java
@@ -4,9 +4,10 @@ public class House {
     double rooms;
 
 
-    public House (String address, int buildYear);
+    public House (String address, int buildYear, double rooms);
     this.address = address;
     this.buildYear = buildYear;
+    this.rooms = rooms;
 
     public String getaddress();
     return address
@@ -14,5 +15,13 @@ public class House {
     public int getbuildyear()
     return buildYear
 
+
+    public static double getRoomSize(int i){
+     system.out.println(int i);
+     return double[] in arr   
+    }
+
+    //b
+    
 
 }
