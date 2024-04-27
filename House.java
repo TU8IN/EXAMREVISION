4d4fac1ed3db7654b73703425a8c230f376591b4
Tuhin Shaikh <shaikhtuhin07@gmail.com>
2024-04-27 17:40:08 +0100

    1

--- House.java
+++ House.java
@@ -4,5 +4,15 @@ public class House {
     double rooms;
 
 
-    public House (String address, int buildYear , double rooms)
+    public House (String address, int buildYear);
+    this.address = address;
+    this.buildYear = buildYear;
+
+    public String getaddress();
+    return address
+
+    public int getbuildyear()
+    return buildYear
+
+
 }
